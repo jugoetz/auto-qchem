f12119b8f3d83066cd2c8eadceaa3cd42589ad31
jugoetz <70372980+jugoetz@users.noreply.github.com>
2021-06-11 09:58:10 +0200

    add LSF support

--- autoqchem/helper_classes.py
+++ autoqchem/helper_classes.py
@@ -19,7 +19,7 @@ class slurm_status(enum.IntEnum):
     done = 3  #: job finished successfully (all gaussian steps finished) and have been retrieved from host
     failed = 4  #: job failed
     incomplete = 5  #: job is incomplete, it should be resubmitted
-    uploaded = 6  #: job has been uploaded to the DB succesfully
+    uploaded = 6  #: job has been uploaded to the DB successfully
 
 
 @dataclass
@@ -62,5 +62,61 @@ class slurm_job:
     directory: str
     base_name: str
     status: slurm_status
+    n_submissions: int
+    n_success_tasks: int
+
+
+@enum.unique
+class lsf_status(enum.IntEnum):
+    """LSF job status enumerator."""
+
+    created = 1  #: job files have been created and stored locally
+    submitted = 2  #: jobs has been submitted to slurm on remote host
+    done = 3  #: job finished successfully (all gaussian steps finished) and have been retrieved from host
+    failed = 4  #: job failed
+    incomplete = 5  #: job is incomplete, it should be resubmitted
+    uploaded = 6  #: job has been uploaded to the DB successfully
+
+
+@dataclass
+class lsf_job:
+    """Dataclass for lsf job.
+
+    :param can: canonical smiles
+    :type can: str
+    :param conformation: conformation number
+    :type conformation: int
+    :param max_num_conformers: max number of conformers generated for the molecule
+    :type: max_num_conformers: int
+    :param tasks: gaussian tasks tuple
+    :type tasks: tuple
+    :param config: gaussian configuration parameters dictionary
+    :type config: dict
+    :param job_id: job id on the remote host
+    :type job_id: int
+    :param directory: job local directory
+    :type directory: str
+    :param base_name: job local base_name
+    :type base_name: str
+    :param status: slurm_status of the job
+    :type status: slurm_status
+    :param n_submission: number of times the job has been submitted
+    :type n_submission: int
+    :param n_success_tasks: number of successfully completed tasks
+    :type n_success_tasks: int
+    """
+
+    # molecule and gaussian config
+    can: str
+    conformation: int
+    max_num_conformers: int
+    tasks: tuple
+    config: dict
+
+    # slurm variables
+    job_id: int
+    directory: str
+    base_name: str
+    status: lsf_status
     n_submissions: int
     n_success_tasks: int
